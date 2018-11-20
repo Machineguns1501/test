db9cf1d609a09933be277a878d99c8c573f76098
Oleg <Machineguns1501@gmail.com>
2018-11-20 23:44:40 +0700

    Это наш первый commit

---
+++ file.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/*
+//Урок 1 - Объявление переменных
+
+console.log(leftBorderWidth);
+//console.log(second);
+
+var leftBorderWidth = 1;
+const pi = 3.14; 
+{
+    let second = 2;
+    console.log(second);
+}
+let second = 3;
+console.log(leftBorderWidth);
+console.log(second);
+*/
+
+/*
+// Урок 2 - Типы данных
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null; 
+undefined;
+var obj = {}; // комплексный тип (сочетание нескольких)
+
+// числовые
+// console.log(4/0);
+// console.log('string'*9);
+
+// console.log(something); //null, поскольку нет объявы и нет переменной
+
+let something;
+console.log(something); //undefined, поскольку нет присвоения данных
+*/
+
+
+// Урок 3 - Общение с пользователем
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone["name"],persone["age"]);//работает в консоли
+
+alert("Hello World!");
+
+confirm("Are you here?");
+
